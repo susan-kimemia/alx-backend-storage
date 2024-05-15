9aede983adebb77729baa72fa0e740ec1076499a
susan-kimemia <susankimemia111@gmail.com>
2024-05-15 12:44:15 +0300

    this

--- 0x02-redis_basic/README.md
+++ 0x02-redis_basic/README.md
@@ -1,1 +1,4 @@
-0x02-redis_basic
+Learning Objectives
+Learn how to use redis for basic operations
+Learn how to use redis as a simple cache
+
--- 0x02-redis_basic/redis-py/dataType-set.py
+++
@@ -1,8 +0,0 @@
-# Add and remove elements from a set
-redis_client.sadd('my_set', 'element1')
-redis_client.sadd('my_set', 'element2')
-
-# Get all elements in the set
-my_set = redis_client.smembers('my_set')
-print(my_set)
-
